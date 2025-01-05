30bb033b9b4a9c74239608f698b662507b5ef8ff
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2025-01-05 20:10:36 +0100

    added test

--- src/App.js
+++ src/App.js
@@ -10,11 +10,36 @@ const App = () => {
             Email address
           </label>
           <input
-            type="eamil"
+            type="email"
             name="email"
             className="form-control"
             id="email"
             aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="password"
+            aria-describedby="passwordHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirm-password" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            name="confirm-password"
+            className="form-control"
+            id="confirm-
+            password"
+            aria-describedby="passwordHelp"
           />
         </div>
         <h1>Registration Form</h1>
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,15 @@
-// filepath: /C:/Users/carri/Documents/GitHub/react-testing/src/App.test.js
+import { act } from "react";
+import react from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("input should be initially empty", () => {
   render(<App />);
-  const emailInput = screen.getByRole("textbox");
-  const passwordInput = screen.getByLabelText(/password/);
-  expect(emailInput.value).toBe("");
-  expect(passwordInput.value).toBe("");
+  const emailInputElement = screen.getByRole("textbox");
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+  expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
 });
